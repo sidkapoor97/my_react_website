7b3a996eb7acc51e2d7773f3a180b992b6cc9d55
Siddharth Kapoor <siddharthakapoor2@gmail.com>
2020-05-13 11:42:53 +0530

    Removed unneccsary break statements

--- src/App.js
+++ src/App.js
@@ -22,14 +22,14 @@ function App() {
       <div className="myPhoto"><img src={myPhoto} alt="" className="my_pic"/></div>
     
     <div className="intro-heading"><h1>Who Am I?</h1></div>
-    <div className="introduction"><p>Senior Undergrad at National Institute Of Technology Karnataka <br />
-    pursing Electronics and Communication with interests in Signal <br />
+    <div className="introduction"><p>Senior Undergrad at National Institute Of Technology Karnataka
+    pursing Electronics and Communication with interests in Signal
     Processing, Deep Learning and Web Development.</p></div>
 
     <div className="doing-heading"><h1>What do I do?</h1></div>
-    <div className="doing"><p>An avid reader, coder and technology enthusiast who <br /> 
-    dabbles in a lot of things. Whether it's a debate on <br />
-    political philosophy or a crazy DIY Project, I am always <br />
+    <div className="doing"><p>An avid reader, coder and technology enthusiast who
+    dabbles in a lot of things. Whether it's a debate on
+    political philosophy or a crazy DIY Project, I am always
     available. </p></div>
 
     <div className="connect">
