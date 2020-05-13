e77eafc47d841f3980b5ec41c80abf1d29e663f5
Siddharth Kapoor <siddharthakapoor2@gmail.com>
2020-05-13 13:19:53 +0530

    removed extra items_up

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -10,7 +10,6 @@ function skills() {
     <div className="skills_app">
     <div><br /></div>
     <div className="skill_heading"><h1>Co-Curricular</h1></div>
-
     <ul className="list_skills">
       <li className="list_item">Joint Convenor, Indian Society for Technical Education NITK
       <br />August 2019 - Till Date</li>
@@ -18,16 +17,15 @@ function skills() {
 
       <li className="list_item">President, Entrpreneurship-Cell NITK<br />
       August 2019 - Till Date</li>
-      <div className="cell"><img src={ecell} alt="" className="ecell-logo" /></div>
+      
 
       <li className="list_item">AR Open Program by Facebook and School Of Innovation <br />
       May 2020 - Ongoing</li>
-      <div className="svco"><img src={svco} alt="" className="svco-logo"/></div>
+      
 
       <li className="list_item">Space Warrior, Under 25<br />
       August 2019 - May 2020</li>
-      <div className="space"><img src={space} alt="" className="space-logo"/></div>
-    <div className="items_up">
+      
       <li className="list_item">Contributions towards Open Source - GNU Radio, DIPY</li>
 
       <li className="list_item">Volunatory Work - Mentor, Avanti Fellows
@@ -35,10 +33,11 @@ function skills() {
 
       <li className="list_item">Languages Known - Python, Javascript, HTML, CSS</li>
       <li className="list_item">Frameworks - Bootstrap, React.js</li>
-
-    </div>
     </ul>
 
+    <div className="cell"><img src={ecell} alt="" className="ecell-logo" /></div>
+    <div className="svco"><img src={svco} alt="" className="svco-logo"/></div>
+    <div className="space"><img src={space} alt="" className="space-logo"/></div>
 
     </div>
   );
