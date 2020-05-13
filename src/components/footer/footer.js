43258cb379183c6089d25f63e7b6c36e5df952bd
Siddharth Kapoor <siddharthakapoor2@gmail.com>
2020-05-13 19:08:37 +0530

    corrected link for footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,7 +10,7 @@ export default function footer() {
                     <br />
                 <p className="contact-me">Contact Me at - <a href="mailto:siddharthakapoor2@gmail.com" alt=""  rel="noopener noreferrer"> Gmail</a></p>
                 <p className="contact-me"><a href="https://github.com/sidkapoor97" alt=""  rel="noopener noreferrer"> GitHub</a></p>
-                <p className="contact-me">Source Code for this website - <a href="" alt="https://github.com/sidkapoor97/my_react_website"  rel="noopener noreferrer">My React Website</a></p>
+                <p className="contact-me">Source Code for this website - <a href="https://github.com/sidkapoor97/my_react_website" alt=""  rel="noopener noreferrer">My React Website</a></p>
                 </div>
             </footer>
         </div>
